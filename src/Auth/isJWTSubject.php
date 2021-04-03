b2b0a6370a119ef0fbce35b04e41328b8889ca62
Louis Charette <charette.louis@gmail.com>
2021-04-03 19:04:37 -0400

    Fix styling

--- src/Auth/isJWTSubject.php
+++ src/Auth/isJWTSubject.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Laravel Auth API
+ *
+ * @author    Louis Charette
+ * @copyright Copyright (c) 2021 Louis Charette
+ * @link      https://github.com/lcharette/laravel-auth-api
+ * @license   https://github.com/lcharette/laravel-auth-api/blob/master/LICENSE.md (MIT License)
+ */
 
 namespace Lcharette\AuthApi\Auth;
 
